a39d55aecfcd43c33e46f5a76738b8d695593466
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-09-28 14:08:12 -0400

    added sample chocolates

--- src/app/chocolates/chocolates.component.ts
+++ src/app/chocolates/chocolates.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../services/auth.service';
 
@@ -8,12 +8,17 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './chocolates.component.html',
   styleUrls: ['./chocolates.component.css']
 })
-export class ChocolatesComponent {
+export class ChocolatesComponent implements OnInit {
   isAdmin = false;
+  myChocolates = ['Buttercreme', 'Vanilla', 'Swirl'];
 
-    // The contructor function runs automatically on component load, each and every time it's called
-    constructor(public as: AuthService) {
-      this.isAdmin = this.as.isAuthed();
-    }
+  // The contructor function runs automatically on component load, each and every time it's called
+  constructor(public as: AuthService) {
+    this.isAdmin = this.as.isAuthed();
+  }
+
+  ngOnInit() {
+
+  }
 
 }
