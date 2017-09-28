cba7c178e9f6ffe545e684347576325af3e14d5d
Dagzy <quincye.williams@gmail.com>
2017-09-28 13:52:37 -0400

    QW merge commit

--- src/app/chocolates/chocolates.component.ts
+++ src/app/chocolates/chocolates.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chocolates.component.css']
 })
 export class ChocolatesComponent implements OnInit {
+  myChocolates = ['Buttercreme','Vanilla','Swirl'];
 
   constructor() { }
 
