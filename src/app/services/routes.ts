0e922e97f1217877ce2e02ccf20e18b87d9dc419
aadwilli <aadwilli15@gmail.com>
2017-09-29 11:02:46 -0400

    added routes

--- src/app/services/routes.ts
+++ src/app/services/routes.ts
@@ -3,7 +3,12 @@ import { AboutComponent } from '../about/about.component';
 import { ChocolatesComponent } from '../chocolates/chocolates.component';
 import { ShopComponent } from '../shop/shop.component';
 import { ContactComponent } from '../contact/contact.component';
-
+import { BarksComponent } from '../barks/barks.component';
+import { CreamsComponent } from '../creams/creams.component';
+import { FudgeComponent } from '../fudge/fudge.component';
+import { MorechocComponent } from '../morechoc/morechoc.component';
+import { SeasonalComponent } from '../seasonal/seasonal.component';
+import { TrufflesComponent } from '../truffles/truffles.component';
 
 export const routes = [
   {
@@ -25,6 +30,30 @@ export const routes = [
   {
     path: 'contact',
     component: ContactComponent
+  },
+  {
+    path: 'barks',
+    component: BarksComponent
+  },
+  {
+    path: 'creams',
+    component: CreamsComponent
+  },
+  {
+    path: 'fudge',
+    component: FudgeComponent
+  },
+  {
+    path: 'morechoc',
+    component: MorechocComponent
+  },
+  {
+    path: 'seasonal',
+    component: SeasonalComponent
+  },
+  {
+    path: 'truffles',
+    component: TrufflesComponent
   },
   {
     path: '**', redirectTo: '/'
