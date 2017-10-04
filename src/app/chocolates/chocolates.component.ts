68d4b5454572ab5572729c79bb9f944d65e542fc
aadwilli <aadwilli15@gmail.com>
2017-10-04 10:02:12 -0400

    choc ts

--- src/app/chocolates/chocolates.component.ts
+++ src/app/chocolates/chocolates.component.ts
@@ -1,10 +1,10 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  AuthService
-} from '../services/auth.service';
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { AuthService } from '../services/auth.service';
+import { ContentService } from '../services/content.service';
+import { Image } from '../services/image';
 
 
 @Component({
@@ -13,26 +13,41 @@ import {
   styleUrls: ['./chocolates.component.css']
 })
 
-export class ChocolatesComponent implements OnInit {
-  items: Array<any> = [];
+export class ChocolatesComponent {
   isAdmin = false;
-  // myChocolates = ['Gourmet Truffles', 'Assorted Creams', 'Barks and Toffee', 'Fudge', 'More Chocolates', 'Seasonal Treats'];
+  selectedFiles: FileList;
+  currentUpload: Image;
+  image1Description: string;
+  image1Src: string;
 
   // The contructor function runs automatically on component load, each and every time it's called
-  constructor(public as: AuthService) {
-    // this.items = [
-    //   { name: 'assets/sampleChocolate.png' },
-    //   { name: 'assets/ganachesign.png' },
-    //   { name: 'assets/sampleChocolate.png' },
-    //   { name: 'assets/chainsign.png' },
-    //   { name: 'assets/sampleChocolate.png' },
-    // ];
+  constructor(public as: AuthService, public cs: ContentService, public fms: FlashMessagesService) {
+    // Admin check
     this.isAdmin = this.as.isAuthed();
+    // Pull updated content from Firebase
+    this.getContent();
   }
 
-ngOnInit() {
+  // Pulls page content from Firebase and assigns it to content based on admin status
+  public getContent() {
+    const thisSaved = this;
+    this.cs.getPageContent('chocolatesPage').then(function (pageContent) {
+      if (thisSaved.isAdmin) {
+        // If they're an admin, set the content of the editors
+        tinymce.get('mainHeader').setContent(pageContent.mainHeader);
+      } else {
+        // Otherwise, set the content of the regularly displayed fields
+        $('#mainHeader').html(pageContent.mainHeader);
+      }
+    });
+  }
 
+  // As an admin, saves the content of the editor for the main header of the page
+  saveMainHeader() {
+    const thisSaved = this;
+    this.cs.savePageContent('chocolatesPage', 'mainHeader', tinymce.get('mainHeader').getContent()).then(function () {
+      thisSaved.fms.show('Main Header Updated', { cssClass: 'alert-success', timeout: 2000 });
+    });
   }
 
 }
-
