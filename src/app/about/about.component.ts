94f1b1096768f4b69274abd245ebfe4355b58ab4
Dagzy <quincye.williams@gmail.com>
2017-10-06 12:48:26 -0400

    QW added gLogo to footer

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -20,6 +20,8 @@ export class AboutComponent {
   image1Description: string;
   image1Src: string;
   uploadingImage1 = false;
+  loginModel = { email: '', pass: '' };
+  loginErrors = { email: '', pass: '' };
 
 
 
@@ -29,7 +31,45 @@ export class AboutComponent {
     this.isAdmin = this.as.isAuthed();
     // Pull updated content from Firebase
     this.getContent();
+    // Wipe any pre-existing login info
+    this.loginModel = { email: '', pass: '' };
+    this.loginErrors = { email: '', pass: '' };
   }
+
+
+  // If the login form isn't completely filled out, set error messages
+    validateLogin() {
+      this.loginErrors = { email: '', pass: '' };
+      if (!this.loginModel.email) {
+        this.loginErrors.email = 'Please provide an email';
+      }
+      if (!this.loginModel.pass) {
+        this.loginErrors.pass = 'Please provide a password';
+      }
+      // Returns true if either is not filled out
+      return (this.loginErrors.email || this.loginErrors.pass);
+    }
+
+
+ // Handles login logic
+    adminLogin() {
+      // If there are errors, do not submit the form
+      if (this.validateLogin()) {
+        return;
+      }
+      // Constructs user to login to firebase with, then login using the service
+      const thisSaved = this;
+      this.as.login(this.loginModel.email, this.loginModel.pass).then(function (err) {
+        if (err !== undefined) {
+          // If there is an error, then display a message to the user
+          thisSaved.loginErrors.pass = err.message;
+        } else {
+          // Successful login!
+          $('.modal-backdrop').remove();
+          location.reload();
+        }
+      });
+    }
 
 
 
