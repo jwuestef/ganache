e0ea2f8586d5c65351377793d5e878727bd3c6e1
Dagzy <quincye.williams@gmail.com>
2017-10-24 15:24:03 -0400

    QW another navbar commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -33,7 +33,9 @@ export class NavbarComponent implements OnInit {
   mobileNav(path) {
     this.router.navigate([path]);
   }
-
+  shopNav(){
+    window.location.href = 'https://shop.ganachechocolatier.org/';
+  }
   ngOnInit() {
 
     // For admin view, if there are TinyMCE editors on the page when it's navigated away from, it will kick errors
